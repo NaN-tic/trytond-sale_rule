8f1b03e5f38870fa16ddf2d927adf24f41c15185
resteve <resteve@zikzakmedia.com>
2015-04-17 08:04:49 +0200

    Add forget files last commit

---
+++ shop.py
@@ -0,0 +1,18 @@
+#This file is part sale_rule module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains
+#the full copyright notices and license terms.
+from trytond.model import fields
+from trytond.pool import PoolMeta
+
+__all__ = ['SaleShop']
+__metaclass__ = PoolMeta
+
+
+class SaleShop:
+    __name__ = 'sale.shop'
+    apply_rules = fields.Boolean('Apply Rules',
+        help='Apply rules when change draft to quotation.')
+
+    @staticmethod
+    def default_apply_rules():
+        return True

bda22804b5e98b7ce048f7197b02f6d22849b1bb
resteve <resteve@zikzakmedia.com>
2015-12-01 11:21:56 +0100

    Tests

--- tests/test_sale_rule.py
+++ tests/test_sale_rule.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the sale_rule module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleRuleTestCase(unittest.TestCase):
+class SaleRuleTestCase(ModuleTestCase):
     'Test Sale Rule module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_rule')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_rule')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_rule'
 
 
 def suite():

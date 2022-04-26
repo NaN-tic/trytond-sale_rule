93014d1bdede5832c4cff0e2928d21d6c90e2c1f
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:12:28 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is part of the sale_rule module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
-from .test_sale_rule import suite
-
-__all__ = ['suite']

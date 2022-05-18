a78e8a2ffc1d4f5e5367089511424b52b70e9712
Jared Esparza <jared.esparza@nan-tic.com>
2022-05-18 14:03:56 +0200

    Use SaleShopCompanyTestMixin

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,11 +2,11 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 
-from trytond.modules.company.tests import CompanyTestMixin
+from trytond.modules.sale_shop.tests import SaleShopCompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleRuleTestCase(CompanyTestMixin, ModuleTestCase):
+class SaleRuleTestCase(SaleShopCompanyTestMixin, ModuleTestCase):
     'Test SaleRule module'
     module = 'sale_rule'
     extras = ['sale_pos', 'sale_discount']
